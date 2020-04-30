be4221deb23a610347a3f0ea72cd2b309551993c
arrudacaio <caio.arruda@ccc.ufcg.edu.br>
2020-04-30 05:20:15 -0300

    edit react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+
+export default function App(){
+  return(
+    <div className="top">
+      <h1>FISCALIZA</h1>
+      <p>consulte os gastos dos parlamentares com mandato em andamento</p>
+    </div>
+
+  );
+
+}
