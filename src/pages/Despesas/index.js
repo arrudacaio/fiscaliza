294099a93f1df70ab0cf63c5309711003d4b5f8f
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-05-12 02:53:00 -0300

    edita css

--- src/pages/Despesas/index.js
+++ src/pages/Despesas/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link  } from 'react-router-dom';
+
 import './styles.css';
 ///import Model from '../../components/Model/index';
 
@@ -7,7 +9,9 @@ export default function Despesas(){
     //<Model title="Despesas do deputado" />
     <>
       <div className="sidebar">
-        <header>FISCALIZA</header>
+        <Link to="/">
+          <header>FISCALIZA</header>
+        </Link>
         <form>
           <h3>Despesas do Deputado:</h3>
           <input type="text" placeholder="Nome do Deputado"  />
@@ -16,7 +20,21 @@ export default function Despesas(){
         </form>
       </div>
       <div class="content">
-        <p>OIIIIII</p>
+        <h1>Despesas</h1>
+        <p>
+          Despesas dos parlamentares com
+          mandato em exercício.
+        </p>
+        <div className="card">
+          <img src="https://www.camara.leg.br/internet/deputado/bandep/76874.jpg" />
+          <div className="card-content">
+            <h1>Marcelo Freixo</h1>
+            <p>Partido: PSOL</p>
+            <h3>Despesas do Deputado em 2020:</h3>
+            <h2>R$ 34.241 Reais</h2>
+          </div>
+        </div>
+        
       </div>
     </>
 
