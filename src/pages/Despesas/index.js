329f17290329188a83e39f3fd7910ac75df9910b
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-05-12 23:00:29 -0300

    finished despesas

--- src/pages/Despesas/index.js
+++ src/pages/Despesas/index.js
@@ -1,21 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link  } from 'react-router-dom';
-
+import api from '../../services/api';
 import './styles.css';
-///import Model from '../../components/Model/index';
+
+
 
 export default function Despesas(){
+  const [deputado, setDeputado] = useState('');
+  const [img, setImg ] = useState('');
+  const [id, setId] = useState('');
+  const [partido, setPartido] = useState('');
+  const [gastos, setGastos] = useState();
+  const [ano, setAno] = useState();
+
+  function somaValores(lista){
+    var total = 0;
+    for(var i = 0; i < lista.length; i++){
+      total += lista[i];
+    } 
+    return total;
+  }
+
+  const getDeputado = async (e) => {
+    try {
+      e.preventDefault();
+
+      const user = e.target.elements.deputado.value;
+      const ano = e.target.elements.ano.value;
+      setAno(ano);
+
+      if(user && ano) {
+        const res = await api.get(`?nome=${user}`);
+        const dados = res.data.dados[0]; 
+        const despesas = await api.get(`/${dados.id}/despesas?ano=${ano}&ordem=ASC&ordenarPor=ano`);
+        const listaDados = despesas.data.dados;
+        
+
+        setId(dados.id);
+        setDeputado(dados.nome);
+        setImg(dados.urlFoto);
+        setPartido(dados.siglaPartido);
+        
+        
+        const gastos = listaDados.map(despesa => {
+          if(despesa.ano === Number(ano)){
+            return despesa.valorLiquido;
+          }
+        });
+
+        var totalGastos = somaValores(gastos);
+        setGastos(totalGastos.toFixed(2));
+
+ 
+
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+
+
   return(
-    //<Model title="Despesas do deputado" />
     <>
       <div className="sidebar">
         <Link to="/">
           <header>FISCALIZA</header>
         </Link>
-        <form>
+        <form onSubmit={getDeputado}>
           <h3>Despesas do Deputado:</h3>
-          <input type="text" placeholder="Nome do Deputado"  />
-          <input type="text" placeholder="Ano das despesas"  />
+          <input type="text" placeholder="Nome do Deputado" name="deputado" />
+          <input type="text" placeholder="Ano das despesas"  name="ano"/>
           <button type="submit">Pesquisar</button>
         </form>
       </div>
@@ -26,12 +83,12 @@ export default function Despesas(){
           mandato em exercício.
         </p>
         <div className="card">
-          <img src="https://www.camara.leg.br/internet/deputado/bandep/76874.jpg" />
+          <img src={img} />
           <div className="card-content">
-            <h1>Marcelo Freixo</h1>
-            <p>Partido: PSOL</p>
-            <h3>Despesas do Deputado em 2020:</h3>
-            <h2>R$ 34.241 Reais</h2>
+            <h1>{deputado}</h1>
+            <p>Partido: {partido}</p>
+            <h3>Despesas do Deputado em {ano}:</h3>
+            <h2>R$ {gastos}</h2>
           </div>
         </div>
         
