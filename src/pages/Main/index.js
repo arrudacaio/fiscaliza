70ba9ca80fcbe362e3a6d77b2a6f90fa38c1fce8
Lucas Medeiros <lucasmed812@gmail.com>
2020-05-16 19:05:12 -0300

    Melhora visualização do valor em reais

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,22 @@
-import React, {useState, Fragment} from 'react';
-import Card from '../../components/Card/Card';
-import currency from 'currency.js';
-import './styles.css';
-import api from '../../services/api';
+import React, { useState, Fragment } from "react";
+import Card from "../../components/Card/Card";
+import currency from "currency.js";
+import "./styles.css";
+import api from "../../services/api";
 
-export default function Main(){
-  const [deputado, setDeputado] = useState('');
-  const [img, setImg ] = useState('');
-  const [id, setId] = useState('');
-  const [partido, setPartido] = useState('');
+export default function Main() {
+  const [deputado, setDeputado] = useState("");
+  const [img, setImg] = useState("");
+  const [id, setId] = useState("");
+  const [partido, setPartido] = useState("");
   const [gastos, setGastos] = useState();
   const [ano, setAno] = useState();
-  
 
-
-  function somaValores(lista){
+  function somaValores(lista) {
     var total = 0;
-    for(var i = 0; i < lista.length; i++){
+    for (var i = 0; i < lista.length; i++) {
       total += lista[i];
-    } 
+    }
     return total;
   }
 
@@ -30,68 +28,67 @@ export default function Main(){
       const ano = e.target.elements.ano.value;
       setAno(ano);
 
-      if(user && ano) {
+      if (user && ano) {
         const res = await api.get(`?nome=${user}`);
-        const dados = res.data.dados[0]; 
-        const despesas = await api.get(`/${dados.id}/despesas?ano=${ano}&ordem=ASC&ordenarPor=ano`);
+        const dados = res.data.dados[0];
+        const despesas = await api.get(
+          `/${dados.id}/despesas?ano=${ano}&ordem=ASC&ordenarPor=ano`
+        );
         const listaDados = despesas.data.dados;
-        
 
         setId(dados.id);
         setDeputado(dados.nome);
         setImg(dados.urlFoto);
         setPartido(dados.siglaPartido);
-        
-        
-        const gastos = listaDados.map(despesa => {
-          if(despesa.ano === Number(ano)){
+
+        const gastos = listaDados.map((despesa) => {
+          if (despesa.ano === Number(ano)) {
             return despesa.valorLiquido;
           }
         });
 
         var totalGastos = somaValores(gastos);
         //setGastos(totalGastos.toFixed(2));
-        setGastos(currency(totalGastos.toFixed(2), { separator: ' ' }).format());
-
- 
-
+        setGastos(
+          currency(totalGastos.toFixed(2), {
+            separator: ".",
+            decimal: ",",
+          }).format()
+        );
       }
-
     } catch (error) {
       console.log(error);
     }
+  };
 
-  }
-
-  return(
+  return (
     <div className="container">
       <div className="logo">
         <h1>FISCALIZA</h1>
         <p>consulte os gastos dos parlamentares com mandato em andamento</p>
       </div>
       <form onSubmit={getDeputado}>
-        <input type="text" placeholder="Nome do deputado" name="deputado"/>
-        <input id="number" type="number" placeholder="Ano" name="ano"/>
+        <input type="text" placeholder="Nome do deputado" name="deputado" />
+        <input id="number" type="number" placeholder="Ano" name="ano" />
         <button type="submit">Pesquisar</button>
       </form>
-      {
-        deputado && ano ?
-        <Card 
+      {deputado && ano ? (
+        <Card
           img={img}
           deputado={deputado}
           partido={partido}
           ano={ano}
           gastos={gastos}
-        /> : <Fragment />
-      }
+        />
+      ) : (
+        <Fragment />
+      )}
       <footer>
-        <p>Todos os dados foram retirados da API de Dados Abertos da Câmara dos Deputados</p>
+        <p>
+          Todos os dados foram retirados da API de Dados Abertos da Câmara dos
+          Deputados
+        </p>
       </footer>
-
     </div>
   );
-
-
-
-
 }
