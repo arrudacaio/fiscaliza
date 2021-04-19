bfb5226fad1430c584d2f1da1e981f421dc39e2d
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-04-19 13:50:54 -0300

    Melhoria na legibilidade das props dos componentes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './styles.css';
 
 
-export default function Card(props){
+export default function Card({deputado, img, partido, ano, gastos}){
 
   return(
     <div className="card">
       <div style={{ minWidth: 114 }}>
-        <img alt={props.deputado} src={props.img} />
+        <img alt={deputado} src={img} />
       </div>
       <div className="infos">
-        <h1>{props.deputado}</h1>
-        <p>Partido: {props.partido}</p>
-        <h3>Despesas do Deputado em {props.ano}:</h3>
-        <h2>R$ {props.gastos} Reais</h2>
+        <h1>{deputado}</h1>
+        <p>Partido: {partido}</p>
+        <h3>Despesas do Deputado em {ano}:</h3>
+        <h2>R$ {gastos} Reais</h2>
       </div>
     </div>
   );
